3b7d69f37ca297404bca5b0abffac08fa07a2153
Valery Schepaschenko <valery.sntx@swanteams.com>
2013-06-04 01:26:37 +0300

    Initial quick and dirty sample project.
    /Home/Pdf for binary pdf, /Home/Viewer/ for pdf.js viewer

---
+++ pdfrenderer/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Rotativa;
+
+namespace pdfrenderer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+          
+
+            return View();
+        }
+
+        public ActionResult Pdf()
+        {
+            var model = new  {DocTitle = "title", DocContent = "This is a test"};
+            return new ViewAsPdf("About",model);
+        }
+
+        public ActionResult Viewer(string url)
+        {
+
+            ViewBag.PdfRawUrl = url;
+            return View();
+
+        }
+
+        
+        public ActionResult Embed(string url)
+        {
+
+            return new ViewAsPdf("Embed",url);
+        }
+        
+
+        public ActionResult Renderer(string url)
+        {
+            ViewBag.PdfRawUrl = url;
+
+            return PartialView();
+        }
+
+        public ActionResult About()
+        {
+            
+            return View();
+        }
+    }
+}
